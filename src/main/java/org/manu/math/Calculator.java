a5b1069e344fefba617ca37dfa2a95a2270e3fc8
Manoj Nair <gmanojnair@gmail.com>
2018-08-20 14:13:41 -0700

    Logger initialised with setting logger level at command line

--- src/main/java/org/manu/math/Calculator.java
+++ src/main/java/org/manu/math/Calculator.java
@@ -1,10 +1,15 @@
 package org.manu.math;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculator {
 	
+	private static Logger LOGGER = LoggerFactory.getLogger(Calculator.class);
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LOGGER.debug("MAIN"+args);
+		System.out.println("Logger initialized");
 
 	}
 
